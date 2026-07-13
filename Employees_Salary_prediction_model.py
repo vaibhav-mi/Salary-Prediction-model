a295590ebdc01c6bfdb1e9d7b38538417b77791c
Vaibhav Mishra <vaibhav48550@gmail.com>
2026-07-13 11:51:21 +0530

    Fix model loading method and clean up code

--- Employees_Salary_prediction_model.py
+++ Employees_Salary_prediction_model.py
@@ -8,7 +8,7 @@ def main():
     html_temp = """<h1>Salary Prediction</h1>"""
     
     model = GradientBoostingRegressor()
-    model.load_model("xgb_model.pkl")
+    model.joblib.load("xgb_model.pkl")
 
     st.markdown(html_temp, unsafe_allow_html = True)
 
